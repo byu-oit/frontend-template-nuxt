53f1b9780084ab4cf55f355ee731612a6814b9e1
Blake Morgan <blake_morgan@byu.edu>
2019-06-10 17:34:28 -0600

    Fix title display

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
+const pkg = require('./package')
+
 export default {
   mode: 'spa',
   /*
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
