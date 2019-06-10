e5faae1b4575b434c8c2b34e09d7812b21c16321
Blake Morgan <blake_morgan@byu.edu>
2019-06-10 15:43:21 -0600

    Fix linting errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,6 +74,7 @@ export default {
     /*
      ** You can extend webpack config here
      */
+    // eslint-disable-next-line
     extend(config, ctx) {}
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,11 @@
         <vuetify-logo />
       </div>
       <v-card>
-        <v-card-title class="headline"
-          >Welcome to the Vuetify + Nuxt.js template</v-card-title
+        <v-card-title
+          class="headline"
         >
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
         <v-card-text>
           <p>
             Vuetify is a progressive Material Design component framework for
@@ -21,9 +23,11 @@
           </p>
           <p>
             If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
+            <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              title="chat"
+            >discord</a>.
           </p>
           <p>
             Find a bug? Report it on the github
@@ -31,8 +35,7 @@
               href="https://github.com/vuetifyjs/vuetify/issues"
               target="_blank"
               title="contribute"
-              >issue board</a
-            >.
+            >issue board</a>.
           </p>
           <p>
             Thank you for developing with Vuetify and I look forward to bringing
@@ -41,16 +44,19 @@
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
           </div>
-          <hr class="my-3" />
+          <hr class="my-3">
           <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+          >Nuxt GitHub</a>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">Continue</v-btn>
+          <v-btn color="primary" flat nuxt to="/inspire">
+            Continue
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
