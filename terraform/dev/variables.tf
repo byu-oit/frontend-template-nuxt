4d05ac0b4d1cf60e23d205fa78bd9604aca2139e
Blake Morgan <blake_morgan@byu.edu>
2019-10-31 10:42:43 -0600

    Move tf variables to file

---
+++ terraform/dev/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {}
+variable "app-name" {}
+variable "url" {
+  default = ""
+}
+variable "repo-name" {}
+variable "branch" {}
+variable "index-doc" {}
+variable "public-dir" {}
+variable "account-cert-arn" {}
+variable "account-r53-zone-id" {}
+variable "price-class" {}
--- terraform/prd/main.tf
+++ terraform/prd/main.tf
@@ -1,16 +1,3 @@
-variable "region" {}
-variable "app-name" {}
-variable "url" {
-  default = ""
-}
-variable "repo-name" {}
-variable "branch" {}
-variable "index-doc" {}
-variable "public-dir" {}
-variable "account-cert-arn" {}
-variable "account-r53-zone-id" {}
-variable "price-class" {}
-
 provider "aws" {
   profile = "default"
   region  = var.region
