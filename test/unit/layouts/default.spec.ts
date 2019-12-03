ee0ab056e071b2f6221f45ece7ef001c1e6994bf
Glen Sawyer <github@snelg.com>
2019-12-02 17:01:34 -0700

    Fix tests with new package updates

--- test/unit/layouts/default.spec.ts
+++ test/unit/layouts/default.spec.ts
@@ -56,16 +56,21 @@ describe('layouts/default', () => {
     expect($store.commit).toHaveBeenCalledWith('clearManualRefresh')
   })
 
-  test('network error', () => {
+  test('network error', async () => {
     const dialog = wrapper.find('.network-errors-dialog')
 
     expect(dialog.html()).toContain('display: none')
+
     $store.state.networkErrors.push('Network error occurred!')
+    await wrapper.vm.$nextTick()
+
     expect(dialog.html()).not.toContain('display: none')
   })
 
-  test('username displayed', () => {
+  test('username displayed', async () => {
     $store.state.username = 'Dummy User'
+    await wrapper.vm.$nextTick()
+
     expect(wrapper.html()).toContain('Dummy User')
   })
 })
