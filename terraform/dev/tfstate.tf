7a4281432aec17476d3314d36d629719eee23274
yoshutch <yoshutch@gmail.com>
2019-12-05 15:35:58 -0700

    changed terraform backend s3 bucket name

--- terraform/dev/tfstate.tf
+++ terraform/dev/tfstate.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "terraform-state-storage-ACCOUNTID"
+    bucket         = "terraform-state-ACCOUNTID"
     dynamodb_table = "terraform-state-lock-ACCOUNTID"
     key            = "APPLICATION-NAME/terraform.tfstate"
     region         = "us-west-2"
