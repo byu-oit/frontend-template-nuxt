ab641736fbf0b37023eff34a87e28ba2e2029fc0
Blake Morgan <blake_morgan@byu.edu>
2020-09-29 12:11:11 -0600

    Update module versions

--- terraform/dev/app/app-dev.tf
+++ terraform/dev/app/app-dev.tf
@@ -34,7 +34,7 @@ locals {
 }
 
 module "acs" {
-  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v3.0.0"
+  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v3.1.0"
 }
 
 data "aws_route53_zone" "hosted_zone" {
@@ -42,7 +42,7 @@ data "aws_route53_zone" "hosted_zone" {
 }
 
 module "s3_site" {
-  source         = "github.com/byu-oit/terraform-aws-s3staticsite?ref=v3.0.0"
+  source         = "github.com/byu-oit/terraform-aws-s3staticsite?ref=v4.0.0"
   site_url       = local.url
   hosted_zone_id = data.aws_route53_zone.hosted_zone.zone_id
   s3_bucket_name = "${local.app_name}.byu.edu"
